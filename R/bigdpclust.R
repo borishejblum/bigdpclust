13684349c3a68a51c8f35d7f833500810ec63881
Boris <boris.hejblum.ensai@gmail.com>
2019-08-22 16:06:03 +0200

    initial commit

---
+++ R/bigdpclust.R
@@ -0,0 +1,9 @@
+#' Gaussian Dirichlet Process Mixture CLustering For Tall Data
+#'
+#'
+#' @author Boris Hejblum, Paul Kirk
+#' @importFrom NPflow DPMGibbsN
+
+bigdpclust <- function() {
+  print("Hello, world!")
+}
